f0a06419efc419d023dd1e0acdbb117805597767
Ana Beatriz Adriano da Silva <ana.github0@gmail.com>
2022-07-06 12:43:08 -0300

    .

---
+++ CSS/style.php
@@ -0,0 +1,96 @@
+<style>
+
+    /* Cofig Globais */
+
+    * {
+        font-family: Arial, sans-serif;
+        padding: 0;
+        margin: 0;
+        color: #16193B;
+        font-size: 20px;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-image: url(../img/img1.png);
+        background-position: center center;
+        background-attachment: scroll;
+        background-repeat: no-repeat;
+        background-size: cover;
+        border-radius: 0;
+        z-index: 1;
+    }
+
+    /* Menu */
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        background-color: #C8C8C8;
+        overflow: hidden;
+        width: 100%;
+    }
+
+    li {
+        float: right;
+    }
+
+    li a {
+        display: block;
+        color: #000;
+        text-align: center;
+        padding: 8px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background-color: #BABCCC;
+        color: white;
+    }
+
+    /* Formulário */
+
+    .form {
+        background-color: white;
+        width: 750px;
+        height: 450px;
+        margin: 200px auto;
+    }
+
+    /* .box {
+        width: 100vw;
+        height: 100vh;
+        background: #0000;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    } */
+
+    /* .box {
+        width: 600px;
+        height: 300px;
+        background: #C8C8C8;
+        border-radius: 17px;
+        padding-top: 40px;
+    } */
+
+    .botao {
+        width: 70px;
+        height: 30px;
+        border: none;
+        background-color: #0D665D;
+        border-radius: 10px;
+    }
+
+    input {
+        border: none;
+        background-color: #EFEFEF;
+        border-radius: 108px;
+        width: 450px;
+    }
+
+    
+</style>
---
+++ Paginas/login.php
@@ -0,0 +1,45 @@
+<?php
+require("../CSS/style.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href=""> Cadastrar</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </nav>
+
+
+    <!-- <div class="container"> -->
+        <form method="post" action="" class="form">
+            <h1>L&nbsp;&nbsp;O&nbsp;&nbsp;G&nbsp;&nbsp;I&nbsp;&nbsp;N</h1>
+            <label for="email">Email:</label>
+            <br>
+            <input type="text" name="email">
+            <span class="nanda">*</span>
+            <br>
+            <br>
+            <label for="senha">Senha:</label>
+            <br>
+            <input type="text" name="senha">
+            <span class="nanda">* </span>
+            <br>
+            <br>
+            <input class="botao" type="submit" value="Login">
+            <p>Não possui conta? <a href="cadUsuario.php">Crie agora</a></p>
+        </form>
+    <!-- </div> -->
+</body>
+
+</html>
