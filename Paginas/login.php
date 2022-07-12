558c9e60306e9768f67333008992b8f974d25917
ANA BEATRIZ ADRIANO DA SILVA <ana.silva68@alunos.sc.senac.br>
2022-07-12 13:52:34 -0300

    .

--- Paginas/login.php
+++ Paginas/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -7,40 +7,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <!-- CSS -->
-    <link rel="stylesheet" href="CSS/styleLogin.css">
-    <link rel="stylesheet" href="CSS/styleHeader.css">
+    <link rel="stylesheet" href="../CSS/styleLogin.css">
+    <link rel="stylesheet" href="../CSS/styleHeader.css">
 </head>
 
 <body>
     <nav class="menu">
         <img src="" alt="Logo">
         <ul>
-            <li><a href="">Home</a></li>
-            <li id="cadastrar"><a href=""> Cadastrar</a></li>
-            <li><a href="">Login</a></li>
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li id="cadastrar">
+                <a href=""> Cadastrar</a>
+            </li>
+            <li>
+                <a href="">Login</a>
+            </li>
         </ul>
     </nav>
-
 
     <!-- <div class="container"> -->
         <form method="post" action="" class="form">
             <h1>L&nbsp;&nbsp;O&nbsp;&nbsp;G&nbsp;&nbsp;I&nbsp;&nbsp;N</h1>
+
             <label for="email">Email:</label>
             <br>
             <input type="text" name="email">
             <span class="nanda">*</span>
             <br>
             <br>
+
             <label for="senha">Senha:</label>
             <br>
             <input type="text" name="senha">
             <span class="nanda">* </span>
             <br>
             <br>
+
             <input class="botao" type="submit" value="Login">
             <p>Não possui conta? <a href="cadUsuario.php">Crie agora</a></p>
         </form>
     <!-- </div> -->
 </body>
-
 </html>
