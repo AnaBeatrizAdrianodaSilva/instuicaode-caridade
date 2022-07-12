40f013240d4ad1986f877a14d239fe2c636053b8
ANA BEATRIZ ADRIANO DA SILVA <ana.silva68@alunos.sc.senac.br>
2022-07-12 16:01:44 -0300

    favicon

--- Paginas/login.php
+++ Paginas/login.php
@@ -6,48 +6,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    
     <!-- CSS -->
     <link rel="stylesheet" href="../CSS/styleLogin.css">
     <link rel="stylesheet" href="../CSS/styleHeader.css">
+    <link rel="stylesheet" href="CSS/styleFooter.css">
 </head>
 
 <body>
-    <nav class="menu">
-        <img src="" alt="Logo">
-        <ul>
-            <li>
-                <a href="">Home</a>
-            </li>
-            <li id="cadastrar">
-                <a href=""> Cadastrar</a>
-            </li>
-            <li>
-                <a href="">Login</a>
-            </li>
-        </ul>
-    </nav>
+    <?php
+    require("../templates/header.php");
+    ?>
 
     <!-- <div class="container"> -->
-        <form method="post" action="" class="form">
-            <h1>L&nbsp;&nbsp;O&nbsp;&nbsp;G&nbsp;&nbsp;I&nbsp;&nbsp;N</h1>
+    <form method="post" action="" class="form">
+        <h1>L&nbsp;&nbsp;O&nbsp;&nbsp;G&nbsp;&nbsp;I&nbsp;&nbsp;N</h1>
 
-            <label for="email">Email:</label>
-            <br>
-            <input type="text" name="email">
-            <span class="nanda">*</span>
-            <br>
-            <br>
+        <label for="email">Email:</label>
+        <br>
+        <input type="text" name="email">
+        <span class="nanda">*</span>
+        <br>
+        <br>
 
-            <label for="senha">Senha:</label>
-            <br>
-            <input type="text" name="senha">
-            <span class="nanda">* </span>
-            <br>
-            <br>
+        <label for="senha">Senha:</label>
+        <br>
+        <input type="text" name="senha">
+        <span class="nanda">* </span>
+        <br>
+        <br>
 
-            <input class="botao" type="submit" value="Login">
-            <p>Não possui conta? <a href="cadUsuario.php">Crie agora</a></p>
-        </form>
-    <!-- </div> -->
+        <input class="botao" type="submit" value="Login">
+        <p>Não possui conta? <a href="cadUsuario.php">Crie agora</a></p>
+    </form>
+
+    <?php
+        require("../templates/footer.php");
+    ?>
 </body>
 </html>
---
+++ templates/footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="CSS/styleFooter.css">
+</head>
+<body>
+    <footer>
+        <div class="contato">
+            <h2>Contato</h2>
+            <p>Amanda - 
+                <a href="mailto:amanda.rondao@alunos.sc.senac.br">amanda.rondao@alunos.sc.senac.br</a>
+            </p>
+            <p>Ana Beatriz - 
+                <a href="mailto:ana.silva68@alunos.sc.senac.br">ana.silva68@alunos.sc.senac.br</a>
+            </p>
+            <p>Isabella - 
+                <a href="mailto:isabella.schmitz@alunos.sc.senac.br">isabella.schmitz@alunos.sc.senac.br</a>
+            </p>
+            <p>João Gabriel - 
+                <a href="mailto:joao.alves1@alunos.sc.senac.br">joao.alves1@alunos.sc.senac.br</a>
+            </p>
+            <p>Lais Betim - 
+                <a href="mailto:lais.betim@alunos.sc.senac.br">lais.betim@alunos.sc.senac.br</a>
+            </p>
+            <p>Nathalia - 
+                <a href="mailto:nathalia.andrade@alunos.sc.senac.br">nathalia.andrade@alunos.sc.senac.br</a>
+            </p>
+        </div>
+        
+        <div class="sobre">
+            <h2>Sobre</h2>
+
+        </div>
+    </footer>
+</body>
+</html>
