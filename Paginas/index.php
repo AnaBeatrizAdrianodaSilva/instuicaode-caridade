c8e167732730c7a819d36a5aec214fef75cb44f6
Raven <ana.github0@gmail.com>
2022-08-05 17:03:51 -0300

    Index pronto

--- Paginas/index.php
+++ Paginas/index.php
@@ -13,6 +13,27 @@
     <link rel="stylesheet" href="../CSS/style.css">
     <link rel="stylesheet" href="../CSS/styleHeader.css">
     <link rel="stylesheet" href="../CSS/styleFooter.css">
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <!-- font-family: 'Quicksand', sans-serif; -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <!-- font-family: 'Poppins', sans-serif; -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+    <!-- font-family: 'Ubuntu', sans-serif; -->
 </head>
 
 <body>
@@ -20,12 +41,128 @@
         require("../templates/header.php");
     ?>
 
-    <header>
+    <header class='cabeca'>
         <h1>Bem Vindo(a)</h1>
+        <p>
+            Instituições de caridade são empresas privadas de caráter não lucrativo com fins de beneficiar determinadas
+            causas.
+        </p>
     </header>
+
+    <div class='row'>
+        <h2>Instituições recomendadas</h2>
+        <a href="./verMais.php">Ver Mais</a>
+    </div>
+
+    <section class='instituicoes'>
+        <figure>
+            <img src="../img/andreLuiz.jpg" alt="Foto da uma das sedes da instituição André Luiz">
+            <figcaption>
+                <h3>Casas André Luiz</h3>
+                <p>
+                    A entidade existe desde 1949 e está localizada em Guarulhos, São Paulo. Eles atendem gratuitamente
+                    mais de 1.400 pacientes com necessidades especiais. A instituição recebe alimentos, roupas,
+                    calçados, eletrodomésticos, eletrônicos e produtos usados em bom estado. A coleta pode ser feita na
+                    casa do doador, porém ele precisa residir na Grande São Paulo. Tudo que é doado é vendido no bazar
+                    beneficente da própria instituição. Mais informações: <a href='tel:+1124597000'>(11) 2459-7000</a>
+                </p>
+            </figcaption>
+        </figure>
+
+        <figure>
+            <img src="../img/abrace.jpg" alt="Foto da uma das sedes da instituição André Luiz">
+            <figcaption>
+                <h3>Abrace</h3>
+                <p>
+                    Em 1986, um grupo de pais cujos filhos faziam tratamento de câncer no Hospital de Base do Distrito
+                    Federal (HBDF), se uniu para ajudar outros pais de crianças que estavam passando pelas mesmas
+                    dificuldades. Assim nasceu a Abrace, Associação Brasileira de Assistência às Famílias de Crianças
+                    Portadoras de Câncer e Hemopatias, com foco em ajudar as famílias que, além da doença, enfrentavam
+                    condições socioeconômicas que agravavam ainda mais esse momento tão delicado. A instituição
+                    contribuiu para que o índice de cura no Distrito Federal aumentasse de 50% para 70%, e o índice de
+                    abandono do tratamento de 28% revertesse para zero. A instituição recebe doações de roupas e
+                    alimentos. A coleta pode ser feita na casa do doador, porém ele precisa residir em Brasília. As
+                    roupas doadas são vendidas no bazar beneficente da própria instituição. Mais informações: <a
+                        href='tel:+6132126000'>(61) 3212-6000</a>
+                </p>
+            </figcaption>
+        </figure>
+
+        <figure>
+            <img src="../img/chicoXavier.jpg" alt="Foto da uma das sedes da instituição André Luiz">
+            <figcaption>
+                <h3>Larzinho Chico e Xavier</h3>
+                <p>
+                    Instituição sediada em Brasília, que tem a missão de acolher, em caráter transitório, crianças e
+                    adolescentes em situação de vulnerabilidade social. Os jovens acolhidos tem a faixa etária de zero a
+                    dezessete anos de idade e são encaminhados pela Vara da infância e da juventude do Distrito Federal.
+                    A associação Lar Infantil Chico Xavier ou Larzinho foi fundada em 1995 e já acolheu mais de 300
+                    crianças e jovens. A instituição recebe alimentos, brinquedos, produtos de higiene, produtos de
+                    limpeza, roupas, dentre outros. Endereço: SMPW Qd. 01, Conjunto 04 Lote 05/C, Park Way, Núcleo
+                    Bandeirante, Brasília, DF. Mais informações: <a href='tel:+61982662713'>(61) 98266-2713</a>
+                </p>
+            </figcaption>
+        </figure>
+
+        <figure>
+            <img src="../img/institutoCrianca.jpg" alt="Foto da uma das sedes da instituição André Luiz">
+            <figcaption>
+                <h3>Instituto da Criança</h3>
+                <p>
+                    Com 26 anos de história, o Instituto funciona como uma via de aproximação entre pessoas físicas e
+                    jurídicas que têm condições e vontade de contribuir, contudo não sabem como fazer este investimento
+                    chegar a quem realmente precisa. Desta forma, a instituição conecta e articula esses dois grupos,
+                    assessorando empresas e direcionando recursos financeiros, humanos, materiais e conhecimentos
+                    técnicos a fim de promover o desenvolvimento social. Anualmente, investe em projetos e campanhas de
+                    educação, cidadania, geração de renda e desenvolvimento comunitário. A instituição tem sede no Rio
+                    de Janeiro e recebe brinquedos novos e usados de segunda a sexta, das 9 às 18 horas, na Casa
+                    Maternal Mello Mattos. Endereço: Rua Faró 80, anexo 2, 2º andar, Jardim Botânico, Rio de Janeiro,
+                    RJ. Mais informações: <a href='tel:+2122399555'>(21) 2239-9555</a>
+                </p>
+            </figcaption>
+        </figure>
+
+        <figure>
+            <img src="../img/exercitoSalvacao.jpg" alt="Foto Exército de Salvação">
+            <figcaption>
+                <h3>Exército de Salvação</h3>
+                <p>
+                    A instituição nasceu no Reino Unido em 1865 para ajudar a suprir as necessidades espirituais e
+                    físicas que afligiam milhares de homens, mulheres e crianças que sofriam com a miséria em Londres na
+                    época. O movimento cresceu rapidamente e hoje atua em 126 países. O Exército de Salvação chegou ao
+                    Brasil em 1922 e tem um trabalho bastante diversificado atendendo crianças, idosos, adolescentes e
+                    mulheres em situação de vulnerabilidade social em unidades assistenciais. A sede da instituição fica
+                    em São Paulo, mas ela atua em várias cidades do Brasil. A Campanha de Natal está no ar e o Exército
+                    de Salvação está recebendo roupas, sapatos e brinquedos. Parte das doações são vendidas no bazar
+                    beneficente da própria instituição. Você pode entrar em contato e pedir a retirada dos objetos em
+                    casa, seja você morador de São Paulo, Rio de Janeiro ou Curitiba. Mais informações: <a
+                        href='tel:40032299'>4003-2299</a>
+                </p>
+            </figcaption>
+        </figure>
+
+        <figure>
+            <img src="../img/oProacao.jpg" alt="Foto de crinaças do O Proação">
+            <figcaption>
+                <h3>O Proação</h3>
+                <p>
+                    Núcleo de composição de parcerias em projetos e ações (O Proação) foi criado em agosto de 2005 em
+                    Belo Horizonte, com a proposta de oferecer a crianças e adolescentes inseridos em um ambiente de
+                    risco social e pessoal, um espaço de desenvolvimento de múltiplas habilidades, promoção da
+                    autoestima, crescimento pessoal e interpessoal, com competência e cidadania. A Entidade é sustentada
+                    por uma rede que compactua com o desejo de assegurar a crianças e adolescentes o acesso, a
+                    permanência e o aprendizado de forma ampla e irrestrita. O foco de atenção de “O Proação” é a
+                    criança e o adolescente, buscando oferecer-lhes condições e oportunidades de construção de um novo
+                    projeto de vida. A instituição recebe fraldas, produtos de limpeza, brinquedos, roupas, alimentos e
+                    outros ítens. Mais Informações <a href='tel:+3136585798'>(31) 3658-5798</a>
+                </p>
+            </figcaption>
+        </figure>
+    </section>
 
     <?php
         require("../templates/footer.php");
     ?>
 </body>
+
 </html>
