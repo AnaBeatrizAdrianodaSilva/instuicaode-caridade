ae3dc4013feb167938d4ae1d1297a929ec0aee7c
isabella <isabellaschmitz00@gmail.com>
2022-08-30 11:05:06 -0300

    alterações

---
+++ include/MySql.php
@@ -0,0 +1,21 @@
+<?php
+    define('HOST', 'localhost');
+    define('PORT', '3306');
+    define('DB', 'pi');
+    define('USER', 'root');
+    define('PASS', '');
+
+    try{
+        //conexao
+        $pdo = new PDO('mysql:host='.HOST.';PORT='.PORT.';dbname='.DB,
+                       USER,
+                       PASS,
+                       array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'));
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                
+    } catch (Exception $e){
+        echo 'Erro';
+    }
+
+
+?>
+
